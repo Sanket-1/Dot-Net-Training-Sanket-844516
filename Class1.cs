7dc9d73fa637cccdec9a7f6335119b401a1c9e88
Sanket Shelke <42107475+Sanket-1@users.noreply.github.com>
2020-02-15 17:47:55 +0530

    MAIL MVC
    mail mvc details fetching

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mail.Models
+{
+    public class InboxViewModel
+    {
+        public List<MailDetail> mlist = new List<MailDetail>();
+
+    }
+    public class MAilDetails
+    {
+        
+            public int MailID;
+            public string MailForm;
+            public string Subject1;
+            public DateTime ReceiveDate;
+            public string Messsage;
+    }
+}
