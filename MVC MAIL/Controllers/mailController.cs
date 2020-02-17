63bd738dc724867851fae92cded301873079d886
Sanket Shelke <42107475+Sanket-1@users.noreply.github.com>
2020-02-17 14:29:34 +0530

    MVC MAIL
    MVC MAIL

---
+++ MVC MAIL/Controllers/mailController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using mail.Models;
+
+namespace mail.Controllers
+{
+    public class mailController : Controller
+    {
+        // GET: mail
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string un, string pass)
+        {
+            ASSESMENTEntities3 ctx = new ASSESMENTEntities3();
+            //MAIL ml = new MAIL();
+            //MailDetail md = new MailDetail();
+            var res = ctx.MAILs.Where(n => n.Username == un && n.password1 == pass).FirstOrDefault();
+            if (res != null)
+            {
+                return Redirect("/mail/Process?ID="+res.UserID);
+
+            }
+            ViewBag.msg = "user id and password not mmatch";
+            return View();
+        }
+        public ActionResult Process()
+        {
+            int id = int.Parse(Request["ID"]);
+            InboxViewModel  model = new InboxViewModel();
+            ASSESMENTEntities3 ctx = new ASSESMENTEntities3();
+            var obj = ctx.MailDetails.Where(u => u.UserID == id);
+            foreach (var o in obj)
+            {
+                                
+                    MailDetail ml = new MailDetail();
+                    ml.MailID = o.MailID;
+                    ml.MailForm = o.MailForm;
+                    ml.Subject1 = o.Subject1;
+                    ml.ReceiveDate = o.ReceiveDate;
+                    ml.Messsage = o.Messsage;
+                    model.mlist.Add(ml);
+            }
+                return View(model);
+            
+
+        }
+        public ActionResult DETAILS()
+        {
+            int id = int.Parse(Request["MID"]);
+            InboxViewModel model = new InboxViewModel();
+            ASSESMENTEntities3 ctx = new ASSESMENTEntities3();
+            var o = ctx.MailDetails.Where(u => u.MailID == id).FirstOrDefault();
+
+            MAilDetails ml = new MAilDetails();
+                ml.MailID = o.MailID;
+                ml.MailForm = o.MailForm;
+                ml.Subject1 = o.Subject1;
+                ml.ReceiveDate = o.ReceiveDate;
+                ml.Messsage = o.Messsage;
+
+            return View(ml);
+        }
+    }
+}
---
+++ MVC MAIL/Model1.Context.cs
@@ -0,0 +1,23 @@
+﻿
+namespace mail
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    
+    public partial class ASSESMENTEntities3 : DbContext
+    {
+        public ASSESMENTEntities3()
+            : base("name=ASSESMENTEntities3")
+        {
+        }
+    
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    
+        public virtual DbSet<MAIL> MAILs { get; set; }
+        public virtual DbSet<MailDetail> MailDetails { get; set; }
+    }
+}
