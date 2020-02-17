7d552cd43d22b6fac7541f986444df7f215b93da
Sanket Shelke <42107475+Sanket-1@users.noreply.github.com>
2020-02-17 14:32:55 +0530

    TRM MVC
    TRM MVC

---
+++ TRM MVC/Controllers/TRM1Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TRM.Controllers
+{
+    public class TRM1Controller : Controller
+    {
+        // GET: TRM1
+        [HttpPost]
+        public ActionResult Index(string re, string sk, DateTime st, DateTime en)
+        {
+            ASSESMENTEntities ctx = new ASSESMENTEntities();
+            TRM tr = new TRM();
+            tr.REQUESTNAME = re;
+            tr.SKILL = sk;
+            tr.STARTDATE = st;
+            tr.ENDDATE = en;
+            ctx.TRMs.Add(tr);
+            ctx.SaveChanges();
+            return View();
+        }
+            public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ TRM MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TRM.MvcApplication" Language="C#" %>
